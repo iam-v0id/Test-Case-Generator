6e89ebe34e463440f2e2d5dac4bccc762828e92e
Suhaas Korampally <korampallysuhaas@gmail.com>
2021-05-04 16:57:05 +0530

    Added flex

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('src'))
+app.use( express.static( 'src' ) );
 
 app.get("/", function(req, res){
     res.sendFile( __dirname + "/index.html" );
